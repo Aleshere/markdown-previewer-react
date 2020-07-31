55dd3f6f28eb20e812e11c7a938e0aaeb6dd14fd
Alessio Burgio <alessioburgio85@gmail.com>
2020-07-31 19:22:23 +0200

    Updates

---
+++ precache-manifest.d80d7b732ff2f444f0cce016eacd0c69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7575def765116e7fb118824b5130c9a7",
+    "url": "/markdown-previewer-react/index.html"
+  },
+  {
+    "revision": "4d9bcb294dd891619287",
+    "url": "/markdown-previewer-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4d9bcb294dd891619287",
+    "url": "/markdown-previewer-react/static/js/2.a7fc540d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/markdown-previewer-react/static/js/2.a7fc540d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72efad1e99ae1821c6c6",
+    "url": "/markdown-previewer-react/static/js/main.0ff01340.chunk.js"
+  },
+  {
+    "revision": "c48dd3d22dfb97ba85a2",
+    "url": "/markdown-previewer-react/static/js/runtime-main.bee44dde.js"
+  }
+]);
